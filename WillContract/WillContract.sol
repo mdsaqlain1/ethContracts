3d9eb59849d85f57dcc5c2aa6a9b12cb18d28ed5
Mohammed Saqlain <md.saqlain3100@gmail.com>
2026-04-28 23:02:37 +0530

    Update WillContract.sol

--- WillContract/WillContract.sol
+++ WillContract/WillContract.sol
@@ -1,1 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract WillContract{
+    address public recipient;
+    address public owner;
+    uint public THRESHOLD = 365 days;
+    uint lastPing;
+
+    modifier _owner{
+        require(msg.sender == owner);
+        _;
+    }
+
+    modifier _recipient{
+        require(msg.sender == recipient);
+        _;
+    }
+
+    constructor(address _ownerAddress, address _recipientAddress){
+        owner = _ownerAddress;
+        recipient = _recipientAddress;
+        lastPing = block.timestamp;
+    }
+
+    function deposit() public payable {
+
+    }
+
+    function transfer() external _recipient{
+        require(block.timestamp - lastPing > THRESHOLD, "Too soon to transfer");
+        (bool success, ) = payable(msg.sender).call{value : address(this).balance}("");
+        require(success, "Transfer failed");
+    }
+
+    function pingIamAlive() external _owner{
+        lastPing = block.timestamp;
+    }
+
+    function changeRecipient(address _recipientAddress) public _owner {
+        recipient = _recipientAddress;
+    } 
+
+    function ownerWithdraw() public _owner{
+        (bool success, ) = payable(owner).call{value : address(this).balance}("");
+        require(success, "Transfer failed");
+    }
+
+
+
+}
